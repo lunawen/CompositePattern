2563ecfe9b5c5d6fff0a888ff1ea221dc3dcc8e6
lunawen <xinw@resonatesolutions.com.au>
2022-03-06 22:35:24 +1100

    add builder pattern

---
+++ CompositePattern/FileSystemBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public class FileSystemBuilder
+    {
+        private DirectoryItem currentDirectory;
+
+        public FileSystemBuilder(string rootDirectory)
+        {
+            Root = new DirectoryItem(rootDirectory);
+            currentDirectory = Root;
+        }
+
+        public DirectoryItem Root { get; }
+
+        public DirectoryItem AddDirectory(string name)
+        {
+            var dir = new DirectoryItem(name);
+            currentDirectory.Add(dir);
+            currentDirectory = dir;
+            return dir;
+        }
+
+        public FileItem AddFile(string name, long fileBytes)
+        {
+            var file = new FileItem(name, fileBytes);
+            currentDirectory.Add(file);
+            return file;
+        }
+
+        public DirectoryItem SetCurrentDirectory(string directoryName)
+        {
+            var dirStack = new Stack<DirectoryItem>();
+            dirStack.Push(this.Root);
+            while(dirStack.Count > 0)
+            {
+                var current = dirStack.Pop();
+                if(current.Name == directoryName)
+                {
+                    currentDirectory = current;
+                    return currentDirectory;
+                }
+                foreach(var item in currentDirectory.Items.OfType<DirectoryItem>())
+                {
+                    dirStack.Push(item);
+                }
+            }
+            throw new InvalidOperationException($"Directory name '{directoryName}' not found!");
+        }
+    }
+}
--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -1,23 +1,39 @@
 ﻿// Client
 
 using CompositePattern;
+using Newtonsoft.Json;
 
-var root = new DirectoryItem("development");
-var proj1 = new DirectoryItem("project1");
-var proj2 = new DirectoryItem("project2");
-root.Add(proj1);
-root.Add(proj2);
+var builder = new FileSystemBuilder("development");
+builder.AddDirectory("project1");
+builder.AddFile("p1f1.txt", 2100);
+builder.AddFile("p1f2.txt", 3100);
+builder.AddDirectory("sub-dir");
+builder.AddFile("p1f3.txt", 4100);
+builder.AddFile("p1f4.txt", 5100);
+builder.SetCurrentDirectory("development");
+builder.AddDirectory("project2");
+builder.AddFile("p2f1.txt", 6100);
+builder.AddFile("p2f2.txt", 7100);
 
-proj1.Add(new FileItem("p1f1.txt", 2100));
-proj1.Add(new FileItem("p1f2.txt", 3100));
-var subDir1 = new DirectoryItem("sub-dir1");
-subDir1.Add(new FileItem("p1f3.txt", 4100));
-subDir1.Add(new FileItem("p1f4.txt", 5100));
-proj1.Add(subDir1);
+Console.WriteLine($"Total size (root) : {builder.Root.GetSizeInKB()}");
+Console.WriteLine(JsonConvert.SerializeObject(builder.Root, Formatting.Indented));
 
-proj2.Add(new FileItem("p2f1.txt", 6100));
-proj2.Add(new FileItem("p2f2.txt", 7100));
+//var root = new DirectoryItem("development");
+//var proj1 = new DirectoryItem("project1");
+//var proj2 = new DirectoryItem("project2");
+//root.Add(proj1);
+//root.Add(proj2);
 
-Console.WriteLine($"Total size (proj2): {proj2.GetSizeInKB()}");
-Console.WriteLine($"Total size (proj1): {proj1.GetSizeInKB()}");
-Console.WriteLine($"Total size (root): {root.GetSizeInKB()}");
+//proj1.Add(new FileItem("p1f1.txt", 2100));
+//proj1.Add(new FileItem("p1f2.txt", 3100));
+//var subDir1 = new DirectoryItem("sub-dir1");
+//subDir1.Add(new FileItem("p1f3.txt", 4100));
+//subDir1.Add(new FileItem("p1f4.txt", 5100));
+//proj1.Add(subDir1);
+
+//proj2.Add(new FileItem("p2f1.txt", 6100));
+//proj2.Add(new FileItem("p2f2.txt", 7100));
+
+//Console.WriteLine($"Total size (proj2): {proj2.GetSizeInKB()}");
+//Console.WriteLine($"Total size (proj1): {proj1.GetSizeInKB()}");
+//Console.WriteLine($"Total size (root): {root.GetSizeInKB()}");
