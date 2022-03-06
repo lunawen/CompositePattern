b27b9259f775202cf0198c633ab8ab24cd29d495
lunawen <xinw@resonatesolutions.com.au>
2022-03-06 22:54:16 +1100

    Use built-in .NET composite

---
+++ CompositePattern/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CompositePattern
+{
+    public static class Extensions
+    {
+        public static IEnumerable<XElement> FindElements(this XElement root, Predicate<XElement> predicate)
+        {
+            var stack = new Stack<XElement>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                foreach (var item in current.Elements())
+                {
+                    stack.Push(item);
+                }
+                if (predicate(current))
+                {
+                    yield return current;
+                }
+            }
+        }
+    }
+}
--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -1,22 +1,10 @@
 ﻿// Client
 
-// Create a tree structure
+using CompositePattern;
+using System.Xml.Linq;
 
-using CompositePattern.Structural;
-
-var root = new Composite("root");
-root.Add(new Leaf("Leaf A"));
-root.Add(new Leaf("Leaf B"));
-
-var comp1 = new Composite("Composite C1");
-comp1.Add(new Leaf("Leaf C1-A"));
-comp1.Add(new Leaf("Leaf C1-B"));
-
-var comp2 = new Composite("Composite C2");
-comp2.Add(new Leaf("Leaf C2-A"));
-comp1.Add(comp2);
-
-root.Add(comp1);
-root.Add(new Leaf("Leaf C"));
-
-root.PrimaryOperation(1);
+var xml = XElement.Load("file-system.xml");
+foreach (var leaf in xml.FindElements(x => !x.HasElements))
+{
+    Console.WriteLine($"*********** LEAF: {leaf.Attribute("name")}, {leaf.Attribute("fileBytes")}");
+}
